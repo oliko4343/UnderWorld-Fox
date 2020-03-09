430220f1f270c18627a5b4cbcf8ac25624d6f38c
oliko4343 <33294822+oliko4343@users.noreply.github.com>
2020-03-09 00:42:11 +0000

    Back-up of Project
    Full Version 1

---
+++ UnderWorld Fox/Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+
+    public GameObject[] weapons;
+    public GameObject weaponHere;
+    public GameObject explosion;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponHere = weapons[Random.Range(0, weapons.Length)];
+        GetComponent<SpriteRenderer>().sprite = weaponHere.GetComponent<SpriteRenderer>().sprite;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            gameObject.SetActive(true);
+
+            Instantiate(explosion, transform.position, transform.rotation);
+
+            
+
+            AudioManager.instance.PlaySFX(3);
+        }
+    }
+
+}
---
+++ UnderWorld Fox/Assets/Scripts/SecretWay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SecretWay : MonoBehaviour
+{
+    private Tilemap theTilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theTilemap = gameObject.GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            theTilemap.color = new Color(theTilemap.color.r, theTilemap.color.g, theTilemap.color.b, 0.5f);
+
+             //0.5 to make it transparent or 0 to make it dissapear 
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            theTilemap.color = new Color(theTilemap.color.r, theTilemap.color.g, theTilemap.color.b, 1f);
+
+        }
+    }
+}
